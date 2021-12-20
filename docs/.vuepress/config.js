e05eac86f24887f13aa8d7d6ad82ceeb31ca97ad
guokairui <wxsms@foxmail.com>
2021-12-20 11:06:49 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bundler: '@vuepress/bundler-webpack',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+  plugins: [
+    '@vuepress/plugin-search',
+  ]
+};
